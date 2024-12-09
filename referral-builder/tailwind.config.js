498ab57968e047a48bf539b3a4810b44e0c4c9f3
Ronald Pisano <ronald.pisano@conduitassociates.com>
2024-12-09 20:06:53 +0800

    get page colors

--- referral-builder/tailwind.config.js
+++ referral-builder/tailwind.config.js
@@ -1,8 +1,21 @@
+import { info } from "autoprefixer";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "rgb(0 0 0)",
+        dark2: "rgb(26 42 58)",
+        primary: "rgb(172 172 172)",
+        secondary: "rgb(201 201 201)",
+        muted: "rgb(234 234 234)",
+        success: "rgb(102 220 125)",
+        warning: "rgb(255 152 0)",
+        danger: "rgb(244 67 54)",
+      },
+    },
   },
   plugins: [],
 };
